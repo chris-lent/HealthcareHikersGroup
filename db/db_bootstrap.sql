9bb1d5a14b5777d88ec41d0e54a8de6b3a0517a5
Christina Lent <christinalent@christinas-mbp.mynetworksettings.com>
2023-04-04 19:01:54 -0400

    updating with edits for dockter

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -1,22 +1,29 @@
 -- This file is to bootstrap a database for the CS3200 project. 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP SCHEMA IF EXISTS `dockter` ;
+CREATE SCHEMA IF NOT EXISTS `dockter` DEFAULT CHARACTER SET latin1 ;
+USE `dockter` ;
 
 -- Create a new database.  You can change the name later.  You'll
 -- need this name in the FLASK API file(s),  the AppSmith 
 -- data source creation.
-create database cool_db;
+create database dockter;
 
 -- Via the Docker Compose file, a special user called webapp will 
 -- be created in MySQL. We are going to grant that user 
 -- all privilages to the new database we just created. 
 -- TODO: If you changed the name of the database above, you need 
 -- to change it here too.
-grant all privileges on cool_db.* to 'webapp'@'%';
+grant all privileges on dockter.* to 'webapp'@'%';
 flush privileges;
 
 -- Move into the database we just created.
 -- TODO: If you changed the name of the database above, you need to
 -- change it here too. 
-use cool_db;
+use dockter;
 
 -- Put your DDL 
 CREATE TABLE test_table (
