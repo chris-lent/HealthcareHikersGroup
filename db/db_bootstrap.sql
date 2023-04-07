c8278a282ebf4dc5e390ea97f766df149acabefa
Connie Too <115683397+c2-code@users.noreply.github.com>
2023-04-07 13:15:22 -0400

    finished ddl

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -32,10 +32,10 @@ CREATE TABLE IF NOT EXISTS patient (
   middle_name VARCHAR(20),
   last_name VARCHAR(20) NOT NULL,
   phone VARCHAR(20),
-  street_address VARCHAR(10)
-  state VARCHAR(100),
-  city VARCHAR(200),
-  zipcode VARCHAR(50),
+  street_address VARCHAR(100),
+  state VARCHAR(50),
+  city VARCHAR(50),
+  zipcode INT,
   policy_id INT,
   insurance_rep_id INT,
   FOREIGN KEY policy_id references insurance_plan (policy_id)
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS insurance_plan (
 );
 
 CREATE TABLE IF NOT EXISTS insurance_rep (
-  insure_rep_ID INT PRIMARY KEY,
+  insure_rep_id INT PRIMARY KEY,
   first_name VARCHAR(20) NOT NULL,
   last_name VARCHAR(20) NOT NULL,
   company_id INT,
@@ -69,21 +69,59 @@ CREATE TABLE IF NOT EXISTS medical_history (
 );
 
 CREATE TABLE IF NOT EXISTS appointment (
+  appoint_id INT PRIMARY KEY,
+  time TIME,
+  year INT,
+  month INT, 
+  day INT,
+  reason_for_visit VARCHAR(200)
+  doc_id INT,
+  patient_id INT,
+  FOREIGN KEY doc_id references medical_professional (doc_id),
+  FOREIGN KEY patient_id references patient (patient_id)
 );
 
 CREATE TABLE IF NOT EXISTS availability (
+  doc_id INT,
+  sched_id INT PRIMARY KEY,
+  day_of_week INT,
+  start_time TIME, 
+  end_time TIME,
+  FOREIGN KEY doc_id references medical_professional (doc_id)
 );
 
 CREATE TABLE IF NOT EXISTS medical_professional (
+  doc_id INT PRIMARY KEY,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  specialty_name VARCHAR(100),
+  admin_id INT,
+  FOREIGN KEY admin_id references healthcare_admin_employee (admin_id)
 );
 
 CREATE TABLE IF NOT EXISTS services (
+  service_id INT PRIMARY KEY,
+  service_name VARCHAR(50)
 );
 CREATE TABLE IF NOT EXISTS healthcare_admin_employee (
+  admin_id INT PRIMARY KEY,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  position VARCHAR(50),
+  center_id INT,
+  FOREIGN KEY center_id references medical_center (center_id)
 );
 CREATE TABLE IF NOT EXISTS medical_center (
+  center_id INT PRIMARY KEY,
+  center_name VARCHAR(50),
+  street_address VARCHAR(50), 
+  state VARCHAR(50),
+  city VARCHAR(50),
+  zipcode INT
 );
 CREATE TABLE IF NOT EXISTS conditions (
+  condition_id INT PRIMARY KEY,
+  name VARCHAR(50)
 );
 -- Add sample data. 
 INSERT INTO test_table
