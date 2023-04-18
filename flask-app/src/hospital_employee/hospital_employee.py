23672516bf33b23a65293a17fba52417feb2a5b8
Christina Lent <christinamlent@gmail.com>
2023-04-18 14:38:35 -0400

    adding routes

--- flask-app/src/hospital_employee/hospital_employee.py
+++ flask-app/src/hospital_employee/hospital_employee.py
@@ -7,7 +7,7 @@ hospital_employee = Blueprint('hospital_employee', __name__)
 
 # Get a patient’s (patient_id) current insurance plan (policy_id)
 @hospital_employee.route('/patient/<patientID>', methods=['GET'])
-def get_customer(patientID):
+def get_insurance(patientID):
     cursor = db.get_db().cursor()
     cursor.execute('''
         SELECT plan_name
@@ -32,3 +32,31 @@ def get_customer(patientID):
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+@hospital_employee.route('/center_offers_services/{center_id}', methods=['POST'])
+def add_new_service():
+
+    # collecting the data from the request object
+    the_data = request.json 
+    current_app.logger.info(the_data)
+
+    # extracting the variable
+    name = the_data['product_name']
+    description = the_data['product_description']
+    price = the_data['product_price']
+    category = the_data['product_category']
+
+    # constructing the query
+    query = 'insert into products (product_name, description, list_price) values ("'
+    query += name + '", "'
+    query += description + '", "'
+    query += category + '", '
+    query += str(price) + ')'
+    current_app.logger.info(query)
+
+    #executing and commiting the inset statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!"
