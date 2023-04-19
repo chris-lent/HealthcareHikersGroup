db2af50fa4ffb748811deaad18b75456ede57926
Christina Lent <christinamlent@gmail.com>
2023-04-19 16:37:38 -0400

    updating to include tests that were lost from fixing the merge

--- flask-app/src/patient/patient.py
+++ flask-app/src/patient/patient.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 from datetime import timedelta
 import json
 from src import db
